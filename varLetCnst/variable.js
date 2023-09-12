3f949b590900ec0e1b4edee39f48129d175f6a34
Mahak Soni <84344169+Mahak129@users.noreply.github.com>
2023-09-12 11:33:36 +0000

    data_types

---
+++ varLetCnst/variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "sonamahae@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2;  cannot be chnaged
+//let and var are introduced to remove the problem of function scope and block scope
+accountEmail="changesMAde.com"
+accountPassword="211211"
+accountCity="Bengluru"
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity]);
