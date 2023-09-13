aa3454bdfde6081ad6d0e67db1795b04b0183b98
Mahak Soni <84344169+Mahak129@users.noreply.github.com>
2023-09-13 09:01:31 +0000

    date

---
+++ 02_count/date.js
@@ -0,0 +1,10 @@
+//dates 
+// let mydate = new Date()//object
+// console.log(mydate); //2023-09-13T08:50:59.443Z
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+
+
+let mycreatedDate = new Date(2023,0,23)
+console.log(mycreatedDate.toDateString());
