a027fbff497962b1c5e21472527f3b6bb75bd55a
Mahak Soni <84344169+Mahak129@users.noreply.github.com>
2023-09-21 15:22:42 +0000

    chng

--- 05_count/filterReducemap.js
+++ 05_count/filterReducemap.js
@@ -3,6 +3,7 @@ const coding =["js","ruby","java","pyhton","cpp"]
 
 const values = coding.forEach((item)=>{
     console.log(item)
+    return values// it do not return anything :---for Each
 })
 
 console.log(values);//returns nothing
