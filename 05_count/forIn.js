e1dd1582dd17e6c208da745166f1573f84858fce
Mahak Soni <84344169+Mahak129@users.noreply.github.com>
2023-09-21 10:55:49 +0000

    for in

--- 05_count/forIn.js
+++ 05_count/forIn.js
@@ -9,18 +9,25 @@ const myObject = {
 // }
 
 for (const key in myObject) {
-console.log(key)
+// console.log(key)
 }
 
 for (const key in myObject) {
  //print values
- console.log(myObject[key])
+//  console.log(myObject[key])
 }
 
 //print both
 for (const key in myObject) {
-console.log(`${key} shortcut is for ${myObject[key]}`)
+// console.log(`${key} shortcut is for ${myObject[key]}`)
+ }
+
+ const programming =["js","rb","py","java","cpp"]
+
+ for(const key in programming){
+    console.log(programming[key])
  }
 
  //for in cannot be applied on map coz its not itreatable
- 
+ //arrays ;-for of
+ //object;-for in
